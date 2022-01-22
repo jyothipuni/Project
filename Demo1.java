4e44f666aa914f3300bd9534eb7d1ddb43c0f9ba
jyothipuni <98212191+jyothipuni@users.noreply.github.com>
2022-01-22 15:39:29 +0530

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,21 @@
+class Demo1
+{
+public static void main(String[] args)
+{
+System.out.println("demo1")
+}
+}
+public class Demo2
+{
+public static void main(String[] args)
+{
+System.out.println("demo2")
+}
+}
+class Demo3
+{
+public static void main(String[] args)
+{
+System.out.println("demo3")
+}
+}
